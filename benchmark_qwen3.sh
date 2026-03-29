6068ea9b0cff47d2c26732b6f826a97274230b26
Goni Zahavy <goni1993@gmail.com>
2026-03-29 20:53:10 +0300

    delete

--- benchmark_qwen3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-quant=${1-bf16}
-
-# Disable OpenMPI ROCm accelerator to prevent segfault on exit
-export OMPI_MCA_accelerator=^rocm
-
-source venv/bin/activate
-mlx_lm.benchmark --model mlx-community/Qwen3-0.6B-$quant -p 4096 -g 128
--- build-venv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-uv venv --clear --seed --python 3.13 venv
-uv pip install --pre torch torchvision --index-url https://download.pytorch.org/whl/nightly/rocm7.2
-uv pip install mlx-lm mlx-vlm
-uv pip install pytest
